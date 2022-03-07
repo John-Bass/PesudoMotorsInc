96bccfd4ba632b1c3b9837d7beb7e7d6c1c0f987
John-Bass <100490655+John-Bass@users.noreply.github.com>
2022-03-07 15:23:07 -0600

    Update PseudoMotorsIncDatabaseStartup.sql

--- Database Code/PseudoMotorsIncDatabaseStartup.sql
+++ Database Code/PseudoMotorsIncDatabaseStartup.sql
@@ -1,25 +1,24 @@
-/*Run the create database first, then delete the create database line. Then Run the whole query and it will create all the tables.*/
-CREATE DATABASE PseudoMotorsInc;
+
 CREATE TABLE customer
 (
-	user_id INT AUTO_INCREMENT,
-    user_userName VARCHAR(40),
-    user_password VARCHAR(12),
-    firstName VARCHAR(20),
+	user_id INT AUTO_INCREMENT,																		
+    user_userName VARCHAR(40),         																 /*UNIQUE & NOT NULL UserName*/
+    user_password VARCHAR(12),																		 /*NOT NULL password*/
+    firstName VARCHAR(20),															/*NOT NULL lName, fName, DOB, address, state, phone*/
     lastName VARCHAR(20),
     DOB DATE,
-    address VARCHAR(40),
+    address VARCHAR(40),			
     state VARCHAR(2),
-    email VARCHAR(60),
-    phone VARCHAR(15),
+    email VARCHAR(60),																					/*UNIQUE & NOT NULL email*/
+    phone VARCHAR(15),	
     PRIMARY KEY(user_id)
 );
 
 CREATE TABLE customerPayment
 (
-	user_id INT,
-    cardType VARCHAR(10),
-    cardNumber NUMERIC(16,0),
+	user_id INT,																		/*UNIQUE AND NOT NULL user_id in CustomerPayment*/
+    cardType VARCHAR(10),																
+    cardNumber NUMERIC(16,0),				
     cardSecurityNumber NUMERIC(3,0),
     PRIMARY KEY(user_id),
     FOREIGN KEY(user_id) REFERENCES customer(user_id) ON DELETE CASCADE
@@ -46,9 +45,9 @@ CREATE TABLE vehicles
 /*Creates rows for the amount of vehicles customer has purchased. Thus, USERID + VEHICLENUMBER allow for finding a specific row.*/
 CREATE TABLE customerVehicles
 (
-	user_id INT,
-    user_vehicleNumber INT,
-    vehicle_id INT,              
+	user_id INT,																			/*NOT NULL user_id in customerVehicles*/
+    user_vehicleNumber INT,          														/*NOT NULL user_vehicleNumber*/
+    vehicle_id INT,              															/*NOT NULL vehicle_id*/
     PRIMARY KEY(user_id, user_vehicleNumber),
     FOREIGN KEY(user_id) REFERENCES customer(user_id) ON DELETE CASCADE,
     FOREIGN KEY(vehicle_id) REFERENCES vehicles(vehicle_id) ON DELETE SET NULL
@@ -57,7 +56,7 @@ CREATE TABLE customerVehicles
 /*Three rows, V4, V6, V8*/
 CREATE TABLE engineWork
 (
-    engineSize VARCHAR(2),
+    engineSize VARCHAR(2),     																	/*UNIQUE engineSize*/
     engineWorkCost INT,
     enginePartsCost INT,
     PRIMARY KEY(engineSize)
@@ -66,7 +65,7 @@ CREATE TABLE engineWork
 /*Two Rows, Auto and Trans*/
 CREATE TABLE transWork
 (
-	transmission VARCHAR(4),
+	transmission VARCHAR(4),																	/*UNIQUE transmission*/
 	transWorkCost INT,
     transPartsCost INT,
     PRIMARY KEY(transmission)
@@ -76,11 +75,11 @@ CREATE TABLE transWork
 CREATE TABLE salesRecieptForVehicles
 (
 	salesRecieptVehicles_id INT AUTO_INCREMENT,
-    user_id INT,
-    vehicle_id INT,
+    user_id INT,																	/*NOT NULL user_id in salesRecieptForVehicles*/
+    vehicle_id INT,																	/*NOT NULL vehicle_id*/
 
-    tax DOUBLE(2,2),
-    totalCost DOUBLE,
+    tax DOUBLE(2,2),																/*NOT NULL tax*/
+    totalCost DOUBLE,																/*NOT NULL totalCost in salesRecieptForVehicles*/
     PRIMARY KEY(salesRecieptVehicles_id),
     FOREIGN KEY(user_id) REFERENCES customer(user_id) ON DELETE SET NULL
 );
@@ -89,11 +88,11 @@ CREATE TABLE salesRecieptForVehicles
 CREATE TABLE salesRecieptForEngineWork
 (
 	salesRecieptEngine_id INT AUTO_INCREMENT,
-    user_id INT,
-    engineInfo VARCHAR(2),
+    user_id INT,													               /*NOT NULL user_id in salesRecieptForEngineWork*/
+    engineInfo VARCHAR(2),															/*NOT NULL engineInfo*/
     
-    tax DOUBLE(2,2),
-    totalCost DOUBLE,
+    tax DOUBLE(2,2),																/*NOT NULL tax*/
+    totalCost DOUBLE,																/*NOT NULL totalCost in salesRecieptForEngineWork*/
     PRIMARY KEY(salesRecieptEngine_id),
     FOREIGN KEY(user_id) REFERENCES customer(user_id) ON DELETE SET NULL,
     FOREIGN KEY(engineInfo) REFERENCES engineWork(engineSize) ON DELETE SET NULL
@@ -103,13 +102,92 @@ CREATE TABLE salesRecieptForEngineWork
 CREATE TABLE salesRecieptForTransWork
 (
 	salesRecieptTrans_id INT AUTO_INCREMENT,
-    user_id INT,
-    transmissionInfo VARCHAR(4),
+    user_id INT,																	/*NOT NULL user_id in salesRecieptForTransWork*/
+    transmissionInfo VARCHAR(4),													/*NOT NULL transmissionInfo*/
     
-    tax DOUBLE(2,2),
-    totalCost DOUBLE,
+    tax DOUBLE(2,2),																/*NOT NULL tax*/
+    totalCost DOUBLE,																/*NOT NULL totalCost in salesRecieptForTransWork*/
     PRIMARY KEY(salesRecieptTrans_id),
     FOREIGN KEY(user_id) REFERENCES customer(user_id) ON DELETE SET NULL,
     FOREIGN KEY(transmissionInfo) REFERENCES transWork(transmission) ON DELETE SET NULL
 );
+
+
+/*
+ALTERS FOR MISSED DATA
+
+NOTICE: SOME ALTERS WILL PRESENT AN ERROR, THIS ISN'T A PROBLEM SINCE THESE ERRORS ALSO OCCURR ON PRIMARY KEY VALUES, THUS THEY WOULD ALREADY
+HAVE TO BE NOT NULL, SO WHEN YOU DO THIS OPERATION ON THEM IT ESSENTIALLY STATES, "No shit sherlock, its a primary key, thus already not null.
+*/
+ALTER TABLE customer CHANGE user_userName user_userName VARCHAR(40) UNIQUE NOT NULL;  
+ALTER TABLE customer CHANGE user_password user_password VARCHAR(12) NOT NULL;  
+ALTER TABLE customer CHANGE firstName firstName VARCHAR(20) NOT NULL;  
+ALTER TABLE customer CHANGE lastName lastName VARCHAR(20) NOT NULL;  
+ALTER TABLE customer CHANGE DOB DOB DATE NOT NULL;  
+ALTER TABLE customer CHANGE address address VARCHAR(40) NOT NULL;  
+ALTER TABLE customer CHANGE state state VARCHAR(2) NOT NULL;  
+ALTER TABLE customer CHANGE email email VARCHAR(60) UNIQUE NOT NULL;  
+ALTER TABLE customer CHANGE phone phone VARCHAR(15) NOT NULL;  
+
+ALTER TABLE customerPayment CHANGE user_id user_id INT UNIQUE NOT NULL;
+
+ALTER TABLE customerVehicles CHANGE user_id user_id INT NOT NULL;
+ALTER TABLE customerVehicles CHANGE user_vehicleNumber user_vehicleNumber INT NOT NULL;
+ALTER TABLE customerVehicles CHANGE vehicle_id vehicle_id INT NOT NULL;
+
+ALTER TABLE engineWork CHANGE engineSize engineSize VARCHAR(2) UNIQUE NOT NULL;
+
+ALTER TABLE transWork CHANGE transmission transmission VARCHAR(4) UNIQUE NOT NULL;
+
+ALTER TABLE salesRecieptForVehicles CHANGE user_id user_id INT NOT NULL;
+ALTER TABLE salesRecieptForVehicles CHANGE vehicle_id vehicle_id INT NOT NULL;
+ALTER TABLE salesRecieptForVehicles CHANGE tax tax DOUBLE(2,2) NOT NULL;
+ALTER TABLE salesRecieptForVehicles CHANGE totalCost totalCost DOUBLE NOT NULL;
+
+ALTER TABLE salesRecieptForEngineWork CHANGE user_id user_id INT NOT NULL;
+ALTER TABLE salesRecieptForEngineWork CHANGE engineInfo engineInfo VARCHAR(2) NOT NULL;
+ALTER TABLE salesRecieptForEngineWork CHANGE tax tax DOUBLE(2,2) NOT NULL;
+ALTER TABLE salesRecieptForEngineWork CHANGE totalCost totalCost DOUBLE NOT NULL;
+
+ALTER TABLE salesRecieptForTransWork CHANGE user_id user_id INT NOT NULL;
+ALTER TABLE salesRecieptForTransWork CHANGE transmissionInfo transmissionInfo VARCHAR(4) NOT NULL;
+ALTER TABLE salesRecieptForTransWork CHANGE tax tax DOUBLE(2,2) NOT NULL;
+ALTER TABLE salesRecieptForTransWork CHANGE totalCost totalCost DOUBLE NOT NULL;
+
+
+/*
+INPUTTING BASE DATA INTO THE TABLES
+*/
+INSERT INTO engineWork VALUES ('V4', 150, 150);
+INSERT INTO engineWork VALUES ('V6', 210, 200);
+INSERT INTO engineWork VALUES ('V8', 275, 245);
+
+INSERT INTO transWork VALUES ('Auto', 200, 130);
+INSERT INTO transWork VALUES ('Manu', 150, 100);
+
+INSERT INTO vehicles VALUES (1, 10, 29000, 'Truck', 'Ford', 'Ford', 'F-150', 2021, 'Red', 'V6', 'AUTO', 5);
+INSERT INTO vehicles VALUES (2, 3, 29000, 'Truck', 'Ford', 'Ford', 'F-150', 2021, 'Blue', 'V6', 'AUTO', 5);
+INSERT INTO vehicles VALUES (3, 8, 29000, 'Truck', 'Ford', 'Ford', 'F-150', 2021, 'Black', 'V6', 'AUTO', 5);
+INSERT INTO vehicles VALUES (4, 4, 29000, 'Truck', 'Ford', 'Ford', 'F-150', 2021, 'White', 'V6', 'AUTO', 5);
+INSERT INTO vehicles VALUES (5, 11, 31000, 'Truck', 'Ford', 'Ford', 'F-150', 2021, 'Red', 'V8', 'AUTO', 5);
+INSERT INTO vehicles VALUES (6, 8, 31000, 'Truck', 'Ford', 'Ford', 'F-150', 2021, 'Blue', 'V8', 'AUTO', 5);
+INSERT INTO vehicles VALUES (7, 6, 31000, 'Truck', 'Ford', 'Ford', 'F-150', 2021, 'Black', 'V8', 'AUTO', 5);
+INSERT INTO vehicles VALUES (8, 3, 31000, 'Truck', 'Ford', 'Ford', 'F-150', 2021, 'White', 'V8', 'AUTO', 5);
+
+SELECT vehicle_id, vehicle_make, vehicle_model, vehicle_engineSize
+FROM vehicles
+WHERE vehicle_make = 'Ford' AND vehicle_model = 'F-150' AND vehicle_engineSize = 'V8';
+
+
+
+
+
+
+
+
+
+
+
+
+
 
